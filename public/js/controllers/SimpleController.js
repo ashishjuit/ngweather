04a21c95f90bae51f3bdc0086fd818a05630fb50
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-07-21 20:52:38 -0400

    added all three services

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -6,6 +6,6 @@
 
   function SimpleController($scope, WeatherService){
     $scope.message = 'Hello There';
-    WeatherService.getHourlyData(29,-82);
+    WeatherService.getDailyData(29,-82);
   }
 })();
